5e1216627e08313e3f7dfbc6d81080ac42515986
AlexandrBiotech <alexandrbiotech@gmail.com>
2025-04-07 15:52:56 +0300

    исправление кода для тестирования метода вычисления площади CalcRectangleArea, размещенного в файле Surname_MathTask.h

--- Polikarpov_Test_MathTask.cpp
+++ Polikarpov_Test_MathTask.cpp
@@ -1,5 +1,6 @@
-#include "CUTE/cute.h"
-#include "CUTE/cute_runner.h"
+#include "CUTE/cute/cute.h"
+#include "CUTE/cute/cute_runner.h"
+#include "CUTE/cute/ide_listener.h"
 #include "Polikarpov_MathTask.h"
 
 using namespace cute;
@@ -7,14 +8,14 @@ using namespace cute;
 void testCalcRectangleArea() {
     int a = 3, b = 5;
     int expected = 15;
-    int actual = CalRectangleArea(a, b);
+    int actual = CalcRectangleArea(a, b);
     ASSERT_EQUAL(expected, actual);
 }
 
 int main() {
     suite s;
     s.push_back(CUTE(testCalcRectangleArea));
-    ide_listener<> lis;
-    makeRunner(lis)(s, "First Test");
+    ide_listener<> listener;
+    makeRunner(listener)(s, "First Test");
     return 0;
 }
